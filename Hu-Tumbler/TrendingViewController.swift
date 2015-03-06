4c3fe308b024154e96809ef4c5d8c6a85e1a81ef
hi-hu <mike@wealthfront.com>
2015-03-06 08:14:53 -0800

    storyboards setup

---
+++ Hu-Tumbler/TrendingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TrendingViewController.swift
+//  Hu-Tumbler
+//
+//  Created by Hi_Hu on 3/6/15.
+//  Copyright (c) 2015 hi_hu. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var trendingSV: UIScrollView!
+    @IBOutlet weak var trendingFeedImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        trendingSV.contentSize = trendingFeedImage.frame.size
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
